28254b08cb26289691c084f4faf8ff7989a80c22
charlene04 <charlesugbana04@gmail.com>
2023-04-14 00:54:33 +0100

    added encode/ route and necessary middlewares to parse request body

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 
+const { encode } = require("./utils/codec");
 
+app.use(express.json());       
+app.use(express.urlencoded({ extended: true }));
 
+app.post('/encode', (req, res) => {
+    let longUrl = req.body.url;
+    return res.status(200).json({ shortUrl: encode(longUrl) }); 
+})
 
 
 app.listen(3000, () => {
